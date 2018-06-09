064adca3b6c8063c707a37ee2528351ecc4dc059
Jose Fernando Lopez Fernandez <jflopezfernandez@gmail.com>
2018-06-09 12:31:30 -0400

    Pausing to keep working on last net program

--- src/src/main.c
+++ src/src/main.c
@@ -1,9 +1,45 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#include <openssl/bio.h>
-#include <openssl/ssl.h>
+#ifdef __cplusplus
+#define NULLPTR nullptr
+#else
+#define NULLPTR NULL
+#endif
+
+#include <openssl/a
+
+//#include <openssl/bio.h>
+//#include <openssl/evp.h>
+//#include <openssl/ssl.h>
+//#include <openssl/x509.h>
+
+typedef int (*spc_x509verifycallback_t)(int, X509_STORE_CTX *);
+
+typedef struct {
+    char *cafile;
+    char *capath;
+    char *crlfile;
+    spc_x509verifycallback_t callback;
+    STACK_OF(X509) *certs;
+    STACK_OF(X509_CRL) *crls;
+    char *use_certfile;
+    STACK_OF(X509) *use_certs;
+    char *use_keyfile;
+    EVP_PKEY *use_key;
+    int *use_key;
+} spc_x509store_t;
+
+BIO* spc_connect_ssl(char *host, int port, spc_x509store_t *spc_store, SSL_CTX **ctx) {
+    BIO *connection = NULLPTR;
+    int our_ctx = 0;
+
+    if (*ctx) {
+        CRYPTO_add(&((*ctx)->references), 1, CRYPTO_LOCK_SSL_CTX);
+    }
+}
 
 int main()
 {
